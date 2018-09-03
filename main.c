51d1f06c4a12962fe6266fff62d7d57c5cb76e23
ALH <asylhawa@rocketmail.com>
2018-09-03 14:57:26 +0100

    layout changes

--- main.c
+++ main.c
@@ -1,31 +1,37 @@
-/*Generalized exact cover.
-This program implements the algorithm discussed in my paper about ``dancing
- links.'' I hacked it together from the {\mc XCOVER} program that I wrote in
-1994; I apologize for not having time to apply spit and polish.
+/* Generalized exact cover.
+ * Donald Knuth
+ * This program implements algorithm X from Dancing Links paper.
+ * Given a 0-1 matrix, the problem is to find all subsets of its rows whose
+   sum is AT MOST 1 in all columns and EXACTLY 1 in all "primary" columns.
 
-Given a matrix whose elements are 0 or 1, the problem is to
-find all subsets of its rows whose sum is at most~1 in all columns and
-{\it exactly\/}~1 in all ``primary'' columns. The matrix is specified
-in the standard input file as follows: Each column has a symbolic name,
-from one to seven characters long. The first line of input contains
-the names of all primary columns, followed by `\.{\char"7C}', followed by
-the names of all other columns.
-(If all columns are primary, the~`\.{\char"7C}' may be omitted.)
-The remaining lines represent the rows, by listing the columns where 1 appears.
-
-The program prints the number of solutions and the total number of link
-updates. It also prints every $n$th solution, if the integer command
-line argument $n$ is given. A second command-line argument causes the
-full search tree to be printed, and a third argument makes the output
-even more verbose.
-*/
+ * Matrix is specific in the standard input file as follows:
+    * Each column has a symbolic name, between 1 and 7 characters long
+    * The first line of input contains the names of all primary columns,
+      followed by '|', followed by the names of all thee other columns.
+    * If all columns are primary, the '|' may be omitted.
+    * The remaining lines represent the rows, by listing the columns
+      where 1 appears.
+ * The program prints the number of solutions and the total number of link
+   updates.
+ * It also prints every 'n'th solution, if the integer command line
+   argument 'n' is given.
+ * A second command line argument causes the full search tree to be printed
+ * A third command line argument makes the output even more verbose.
+ */
 
 #define maxLevel 150 // at most this many rows in a solution
 #define maxDegree 10000 // at most this many branches per search tree node
 #define maxCols 10000 // at most this many columns
 #define maxNodes 1000000 // at most this many nonzero elements in the matrix
 #define verbose Verbose // kludge because of 64-bit madness in SGB library
+
+/* One 'column' struct is called the 'root'.
+ * It serves as the head of the list of columns that need to be covered,
+   and is identifiable by the fact that its 'name' is empty.
+ */
 #define root colArray[0] // gateway to the unsettled columns
+
+
 #define bufSize 8 * maxCols + 3 // upper bound on the input line length
 #define panic(m) { fprintf(stderr, "%s!\n%s", m, buf); exit(-1); }
 
@@ -35,7 +41,35 @@ even more verbose.
 #include <string.h>
 
 
-//Type Definitions
+/* Data structures:
+ * Each column of the input matrix is represented by a 'column' struct.
+ * Each row of the input matris is represented as a linked list of 'node' structs.
+ * There is one node for each non-zero entry in the matrix.
+ * The nodes are linked circularly within each row, in both directions.
+ * The nodes are also linked circularly within each column.
+ * The column lists each include a header node, but the rows lists do not.
+ * Column header nodes are part of a 'column' struct, which contains further
+   information about the column.
+ */
+
+/* Each node contains five fields.
+   * Four are the pointers of doubly linked lists
+   * The fifth points to the column containing the node
+ */
+
+/* Each 'column' struct contains five fields.
+   * The 'head' is a node that stands at the head of its list of nodes.
+   * The 'len' tells the length of that list of nodes, not counting the header.
+   * The 'name' is a user specified identifier.
+   * 'next' and 'prev' point to adjacent columns, when this column is part
+     of a doubly linked list.
+ * As backtracking proceeds, nodes will be deleted from column lists when
+   their row has been blocked by other rows in the partial solution.
+ * When backtracking is complete, the data structures will be restored to
+   their original state.
+ */
+
+// Type Definitions:
 typedef struct nodeStruct{
     struct nodeStruct *left, *right; //predecessor and successor in row
     struct nodeStruct *up, *down; //predecessor and successor in column
@@ -51,7 +85,7 @@ typedef struct colStruct{
 
 
 
-//Global Variables
+// Global Variables:
 int verbose; // $>0$ to show solutions, $>1$ to show partial ones too
 long long count = 0; // number of solutions found so far
 double updates; // number of times we deleted a list element
@@ -69,8 +103,14 @@ int level; // number of choices in current partial solution
 node *choice[maxLevel]; //the row and column chosen on each level
 
 
+// Subroutines:
 
-// Subroutines
+/* Output a row to screen:
+ * A row is identified not by name, but by the names of the columns it contains.
+ * printRow outputs a row, given a pointer to any of its columns.
+ * It also outputs the position of the row in its column.
+ */
+
 void printRow(p)
     node *p;
 {
@@ -101,12 +141,17 @@ void printState(int lev){
     }
 }
 
+/* Covering a column:
+ * When a row is blocked, it leaves all lists except the list of the column
+   that is being covered.
+ * Therefore, a node is never removed from a list twice.
+ */
 void cover(c) // Covering column
     column *c;
 {
     register column *l, *r;
     register node *rr, *nn, *uu, *dd;
-    register k = l; // updates
+    register k = 1; // updates
     l = c->prev;
     r = c->next;
     l->next = r;
@@ -126,6 +171,12 @@ void cover(c) // Covering column
     updateProfile[level] += k;
 }
 
+
+/* Uncovering a column:
+ * Uncovering a column is done in precisely the reverse order.
+ * The pointers thereby execute a 'dance' which returns them
+   to their former state.
+ */
 void uncover(column *c)
     //column *c;
 {
@@ -145,6 +196,7 @@ void uncover(column *c)
 }
 
 
+// Main function:
 
 int main (argc, argv)
     int argc;
@@ -165,6 +217,7 @@ int main (argc, argv)
         sscanf(argv[1], "%d", &spacing);
     }
 
+    // Inputting the matrix - read the column names:
     currentCol = colArray + 1;
     fgets(buf, bufSize, stdin);
     if(buf[strlen(buf)-1] != '\n'){
@@ -212,6 +265,7 @@ int main (argc, argv)
         (currentCol-1)->next = &root, root.prev = currentCol - 1;
     }
 
+    // Inputting the matrix - read the rows:
     currentNode = nodeArray;
     while(fgets(buf, bufSize, stdin)){
         register column *ccol;
@@ -260,8 +314,27 @@ int main (argc, argv)
         rowStart->left = currentNode -1, (currentNode -1)->right = rowStart;
     }
 
+
+    /* Backtracking through all solutions:
+     * Strategy for generating all exact covers will be to repeatedly always choose
+       the column that appears to be hardest to cover from all columns that still
+       need to be covered.
+     * This is the column with the shortest list.
+     * All possibilites are then explored via depth-first search.
+
+     * Depth-first search means last-in-first out maintenance of data structures.
+     * There is no need for auxiliary tables to undelete elements from lists
+       when backing up.
+     * The nodes removed from doubly linked lists remember their former neighbours,
+       because there is no garbage collection.
+
+     * The basic operation is 'covering a column'.
+     * This means removing it from the list of columns needing to be covered,
+       and 'blocking' its rows, which is done by removing nodes from other
+       lists whenever they belong to a row of a node in this column's list.
+     */
     level = 0;
-    forward:
+    forward: ; // Set bestCol to the best column for branching:
         minLen = maxNodes;
         if(verbose > 2){
             printf("Level %d", level);
@@ -292,9 +365,11 @@ int main (argc, argv)
                 printf("branching on %s(%d)\n", bestCol->name, minLen);
             }
         }
+
         cover(bestCol);
         currentNode = choice[level] = bestCol->head.down;
-    advance:
+
+    advance: ;
         if(currentNode == &(bestCol->head)){
             goto backup;
         }
@@ -302,10 +377,12 @@ int main (argc, argv)
             printf("L%d:",level);
             printRow(currentNode);
         }
+        // Cover all other columns of currentNode
         for(pp = currentNode->right; pp != currentNode; pp = pp->right){
             cover(pp->col);
         }
-        if(root.next == &root){
+
+        if(root.next == &root){ //Record solution and goto recover
             count++;
             if(verbose){
                 profile[level+1][0]++;
@@ -329,26 +406,35 @@ int main (argc, argv)
         currentNode = choice[level];
         bestCol = currentNode->col;
 
-    recover:
+    recover: // Uncover all other columns of currentNode
+        /* We included left links, thereby making the rows doubly linked,
+           so that columns would be uncovered in the correct LIFO order
+           in this part of the program.
+         * The 'uncover' routine itself could have done its job with
+           right links only.
+         */
         for(pp = currentNode->left; pp != currentNode; pp = pp->left){
             uncover(pp->col);
         }
+
+
         currentNode = choice[level] = currentNode->down;
         goto advance;
 
     done:
-        if(verbose > 3){
+        if(verbose > 3){ // Print column lengths to make sure everything has been restored
             printf("Final column lengths");
             for(currentCol = root.next; currentCol != &root; currentCol = currentCol->next){
                 printf(" %s(%d)", currentCol->name, currentCol->len);
             }
             printf("\n");
         }
+    // End backtracking
 
     printf("Altogether %lld solutions, after %.15g updates. \n", count, updates);
-    if(verbose){
+    if(verbose){ // Print a profile of the search tree
         double tot, subtot;
-        tot = 1; // the root node does not show up in the profile
+        tot = 1; // The root node does not show up in the profile
         for(level = 1; level <= maxl + 1; level++){
             subtot = 0;
             for(k = 0; k <= maxb; k++){
