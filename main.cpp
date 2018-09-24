6c060d3a6455a35e78515390704e4339517b7212
ALH <asylhawa@rocketmail.com>
2018-09-24 12:29:33 +0100

    removed printRow call from end, moved to recursiveSearch function

--- main.cpp
+++ main.cpp
@@ -84,7 +84,7 @@ int main (int argc, char** argv) {
 
     // Reading rows from file
     currentNode = nodeArray;
-    std::cout << "Enter Row: ";
+    std::cout << "Enter Rows:" << std::endl;
     int x = 0;
     while (x < 6) {
         fgets(buf, BUF_SIZE, stdin);
@@ -153,108 +153,13 @@ int main (int argc, char** argv) {
     int level = 0;
     recursiveSearch(level, currentNode, bestCol);
 
-    for(int i = 0; i < 5; ++i){
-        printRow(choice[i]);
-    }
+
 
 
 
 
 
 } //END INT MAIN
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*forward: ; // Set bestCol to the best column for branching:
-        minLen = MAX_NODES;
-        for(currentCol = root.next; currentCol != &root; currentCol = currentCol->next){
-            if(currentCol->len < minLen){
-                bestCol = currentCol, minLen = currentCol->len;
-            }
-        }
-        if(level > maxl){
-            if(level >= MAX_LEVEL){
-                cout << "Too many levels." << endl;
-                exit(1);
-            }
-            maxl = level;
-        }
-        if(minLen > maxb){
-            if(minLen >= maxDegree){
-                cout << "Too many branches." << endl;
-                exit(1);
-            }
-            maxb = minLen;
-        }
-        ++profile[level][minLen];
-        cover(bestCol);
-        currentNode = choice[level] = bestCol->head.down; //currentNode is first Node in bestCol column
-
-    advance: ;
-        if(currentNode == &(bestCol->head)){ //If all nodes in bestCol column have been assessed
-            goto backup;
-        }
-        for(rowNode = currentNode->right; rowNode != currentNode; rowNode = rowNode->right){
-            cover(rowNode->col);
-        }
-        if(root.next == &root){ //If root column is the only column left (all other columns have been covered)
-            ++count;
-            goto recover;
-        }
-        ++level;
-        goto forward;
-
-    backup: ;
-        uncover(bestCol);
-        if(level == 0){
-            goto done;
-        }
-        --level;
-        currentNode = choice[level];
-        bestCol = currentNode->col;
-
-    // Uncover all other columns of currentNode in the correct LIFO order
-    recover:
-        for(rowNode = currentNode->left; rowNode != currentNode; rowNode = rowNode->left){
-            uncover(rowNode->col);
-        }
-        currentNode = choice[level] = currentNode->down;
-        goto advance;
-
-    done: ;
-        cout << "Final column lengths:" << endl;
-        for(currentCol = root.next; currentCol != &root; currentCol = currentCol->next){
-            cout << currentCol->name << "(" << currentCol->len << ")" << endl;
-        }
-        cout << endl;*/
-
-    /*double tot, subtot;
-    tot = 1; // The root node does not show up in the profile
-    for(level = 1; level <= maxl + 1; ++level){
-        subtot = 0;
-        for(k = 0; k <= maxb; ++k){
-            cout << profile[level][k];
-            subtot += profile[level][k];
-        }
-        //cout << subtot << " nodes, " << updateProfile[level-1] << endl;
-        tot += subtot;
-    }
-    cout << "Total " << tot << "nodes." << endl;*/
 
 
 /* Backtracking through all solutions:
