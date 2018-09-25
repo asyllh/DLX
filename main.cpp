ee1abc77b060e15af45618880acedd5d3ec0a371
ALH <asylhawa@rocketmail.com>
2018-09-25 17:20:04 +0100

    changed to take file input from argument rather than typing in file name in code

--- main.cpp
+++ main.cpp
@@ -5,24 +5,7 @@ main.cpp
 01/09/18
 /--------------*/
 /** TO DO **/
-/* DONE: Make all global functions local, and add necessary arguments to functions
- * DONE: Make header file
- * DONE: Check if all macros are needed
- * nullptr
- * DONE: Remove need for labels
- * DONE: Change code so that it doesn't include 'primary' and other columns, all columns should be needed
-   This involves the '|' symbol that would appear in an input file, change the code so that it doesn't
-   separate primary and non primary columns
- * DONE: Use std::string instead of char pointers
- * DONE: Change method of reading input file, use ifstream/getline etc
- * DONE: Look out for need for null character '\0', might be needed
- * DONE: Check which variables are actually needed
- * DONE: Remove verbose
- * Program arguments
- * Make sure when running this main.cpp file that it is not still linked to the main.c file or dlx.cpp
- * Use try...catch and error functions to end program with message
- * How is the solution being stored? Just want to store which subset of rows contain exactly one 1 in every column
- * Is the output using choice[level] a feasible solution? How do we check?
+/* Is the output using choice[level] a feasible solution? How do we check?
  * How do we label the columns/rows in EVOL, i.e. how to label strips A, B, C or 0, 1, 2,..
    and does it need to be a string or a char?
  * How do we convert output from EVOl to use in DLX?
@@ -36,10 +19,16 @@ main.cpp
 
 int main (int argc, char** argv) {
 
+    if(argc < 2){
+        ProgramInfo();
+        exit(1);
+    }
+
     Timer timer;
 
     Column* currentCol = colArray + 1;
-    std::ifstream ifs("dlxtest.txt");
+    std::ifstream ifs;
+    ifs.open(argv[1]);
     if(!ifs){
         std::cerr << "Cannot open file." << std::endl;
         exit(1);
@@ -103,7 +92,7 @@ int main (int argc, char** argv) {
 
     //Start of search(k) recursive procedure
     int level = 0;
-    recursiveSearch(level, currentNode, bestCol);
+    RecursiveSearch(level, currentNode, bestCol);
 
 
 } //END INT MAIN
